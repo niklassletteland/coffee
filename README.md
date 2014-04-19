7f55c11aab411dcd28ee75aa307f30c3e39e2101
Niklas Sletteland <niklassletteland@gmail.com>
2014-04-18 17:02:27 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+![coffee](https://i.cloudup.com/OPTKt5svqO-3000x3000.png)
+                                                 
+for learning the staples of unit testing / TDD/BDD / E2E testing, and CI
---
+++ server.js
@@ -0,0 +1,30 @@
+/*
+ * @filename server.js
+ * @author Niklas Sletteland
+ *
+ * 
+ *
+ */
+
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res) {
+	res.sendfile('app/index.html')
+});
+
+/*
+app.post("/user/add", function(req, res) { 
+	res.send("OK");
+});
+
+app.get(/^(.+)$/, function(req, res){ 
+ console.log('static file request : ' + req.params);
+ res.sendfile( __dirname + req.params[0]); 
+});
+*/
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+	console.log("started coffee app on port " + port);
+});
