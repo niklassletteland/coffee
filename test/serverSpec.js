392d3ac1b1bd8745c604de730106425c4d3e5fe2
Niklas Sletteland <niklassletteland@gmail.com>
2014-04-21 15:39:18 -0700

    adding some unit testing for coverage

--- test/serverSpec.js
+++ test/serverSpec.js
@@ -3,11 +3,11 @@ var server = require("../server.js");
  
 describe("Server", function(){
 
-	/*describe('#test', function() {
+	describe('#test', function() {
 		it('should return "hello"', function() {
 			var result = server.test();
 			expect(result).to.equal('hello');
 		});
-	});*/
+	});
 
 });
